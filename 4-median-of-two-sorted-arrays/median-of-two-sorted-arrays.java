1393a884a779189ab62822daacd5492f188984ce
Arpita Baraskar <161856961+ArpitaBaraskar@users.noreply.github.com>
2025-08-15 20:09:28 +0530

    Time: 2 ms (56.00%) | Memory: 46.2 MB (44.49%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -2,30 +2,35 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
-        int[] temp = new  int[m+n];
-        int i = 0;
-        int j = 0;
-        int k = 0;
-
-        while(i < m && j < n){
-            if(nums1[i] < nums2[j]){
-              temp[k++] = nums1[i++];
-            }else{
-                temp[k++] = nums2[j++];
+        int[] merged = new int[m + n];
+        
+        int i = 0, j = 0, k = 0;
+        
+        // Merge the two sorted arrays
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
             }
         }
-        while(i < m){
-            temp[k++] = nums1[i++];
-        }
-        while(j < n){
-            temp[k++] = nums2[j++];
+        
+        // Add remaining elements from nums1
+        while (i < m) {
+            merged[k++] = nums1[i++];
         }
         
-        int mid = (m+n)/2;
-        if((m+n) % 2 == 0){
-            return (double)(temp[mid]+temp[mid-1])/2;
-        }else{
-            return (double)temp[mid];
+        // Add remaining elements from nums2
+        while (j < n) {
+            merged[k++] = nums2[j++];
+        }
+        
+        // Find median
+        int total = m + n;
+        if (total % 2 == 1) {
+            return merged[total / 2]; // odd length → middle element
+        } else {
+            return (merged[total / 2 - 1] + merged[total / 2]) / 2.0; // even length → average of two middle elements
         }
     }
 }
