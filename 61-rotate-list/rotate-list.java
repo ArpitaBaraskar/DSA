b16396d7e308bd874f7e2b3a4a3c88dac83395f1
Arpita Baraskar <161856961+ArpitaBaraskar@users.noreply.github.com>
2025-09-09 12:43:06 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (5.31%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -1,30 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if (head == null || head.next == null || k == 0) return head;
-
-        // Step 1: Find length and tail
+         if (head == null || head.next == null || k == 0) return head;
         int length = 1;
         ListNode tail = head;
-        while (tail.next != null) {
+        while(tail.next != null){
             tail = tail.next;
             length++;
         }
-
-        // Step 2: Handle k larger than length
         k = k % length;
-        if (k == 0) return head;
+        if(k == 0) return head;
 
-        // Step 3: Make circular
+        
+
         tail.next = head;
+        int Steps = length - k -1;
 
-        // Step 4: Find new tail = (length - k - 1) steps from head
-        int stepsToNewTail = length - k - 1;
         ListNode newTail = head;
-        while (stepsToNewTail-- > 0) {
+
+        while(Steps-- > 0){
             newTail = newTail.next;
+
         }
 
-        // Step 5: Break circle
         ListNode newHead = newTail.next;
         newTail.next = null;
 
