662ee89c4b531eec89ed42b4d5fca37f95606bc2
Arpita Baraskar <161856961+ArpitaBaraskar@users.noreply.github.com>
2025-09-16 21:56:19 +0530

    Time: 9 ms (96.30%) | Memory: 57.3 MB (16.53%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -1,23 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode sortList(ListNode head) {
         if(head == null || head.next == null) return head;
 
         ListNode mid = getMiddle(head);
         ListNode right = mid.next;
-        mid.next = null;   // break into two halves
+        mid.next = null;
 
         ListNode leftSort = sortList(head);
         ListNode rightSort = sortList(right);
 
         return merge(leftSort, rightSort);
+
     }
 
     public ListNode getMiddle(ListNode head){
         ListNode slow = head, fast = head.next;
+
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
+
         return slow;
     }
 
@@ -26,7 +39,7 @@ class Solution {
         ListNode curr = dummy;
 
         while(l1 != null && l2 != null){
-            if(l1.val < l2.val){   // ascending
+            if(l1.val < l2.val){
                 curr.next = l1;
                 l1 = l1.next;
             }else{
@@ -36,8 +49,15 @@ class Solution {
             curr = curr.next;
         }
 
-        if(l1 != null) curr.next = l1;
-        if(l2 != null) curr.next = l2;
+        if(l1 != null){
+            curr.next = l1;
+            //l1 = l1.next;
+        }
+
+        if(l2 != null){
+            curr.next = l2;
+           // l2 = l2.next;
+        }
 
         return dummy.next;
     }
