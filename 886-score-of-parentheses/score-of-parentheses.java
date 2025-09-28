ff1249cc12a447b9406b68a6503d8375c7ebca42
Arpita Baraskar <161856961+ArpitaBaraskar@users.noreply.github.com>
2025-09-28 14:31:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (79.93%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.java
+++ 886-score-of-parentheses/score-of-parentheses.java
@@ -1,22 +1,21 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        int total = 0;
+        int score = 0;
+        int depth = 0;
 
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (s.substring(i, i + 2).equals("()")) {
-                // prefix till i
-                String p = s.substring(0, i);
-                // depth = count('(') - count(')')
-                int d = 0;
-                for (char c : p.toCharArray()) {
-                    if (c == '(') d++;
-                    else d--;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                depth++;  
+            } else {
+                depth--;  
+                if (s.charAt(i - 1) == '(') {
+                    score += 1 << depth;  // add 2^depth
                 }
-                int score = 1 << d; // same as 2^d
-                total += score;
             }
         }
 
-        return total;
+        return score;
     }
 }
